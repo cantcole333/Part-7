25a3a96bad99f49dbe2f46862f16f581b527c006
Cole Cantwell <cole>
2020-10-12 22:48:58 -0400

    Arrays

--- Part 7/Program.cs
+++ Part 7/Program.cs
@@ -10,6 +10,21 @@ namespace Part_7
     {
         static void Main(string[] args)
         {
+            string[] names = new string[2];
+
+            //names[0] = "John Doe";
+            //names[1] = "Jane Doe";
+
+            //for (int i =0; i < names.Length; i++)
+            //Console.WriteLine("Item number " + i + ": " + names[i]);
+            Random generator = new Random();
+            int[] numbers = new int[30];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = generator.Next(1, 101);
+            foreach (int i in numbers)
+                Console.Write($" {i}"); 
+            Console.ReadLine();
         }
     }
 }
